85f3d7e9acb5c58f8e164c03b7250d09202aec76
ZaicevDima <zaicevdmitiy@gmail.com>
2021-09-20 00:38:51 +0300

    Initial commit. Added Map for station dictionary

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStream in = Files.newInputStream(Path.of("resources\\ghcnd-stations.txt"));
+        StationDictionary dictionary = new StationDictionary(in);
+        Map<String, List<Double>> stationDictionary = dictionary.getStationDictionary();
+
+        //System.out.println(stationDictionary.size());
+    }
+
+}
---
+++ src/StationDictionary.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class StationDictionary {
+    private InputStream in;
+
+    StationDictionary(InputStream in) {
+        this.in = in;
+    }
+
+    public Map<String, List<Double>> getStationDictionary() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        Map<String, List<Double>> stationDictionary = new HashMap<>();
+        String tmp;
+        while ((tmp = reader.readLine()) != null){
+            String[] splittingLine = tmp.split("\s+");
+            stationDictionary.put(splittingLine[0], new ArrayList<>(Arrays.asList(
+                    Double.parseDouble(splittingLine[1]),
+                    Double.parseDouble(splittingLine[2]))));
+        }
+
+        return stationDictionary;
+    }
+}
